272d6154d238b29ee0f0dc46b912bae3195fb991
ayunav <ayunanyc@gmail.com>
2016-04-15 16:38:58 -0400

    add a UILabel and a UIButton programmatically. Set frames and textalignment

--- AVClickCounter/ViewController.swift
+++ AVClickCounter/ViewController.swift
@@ -10,14 +10,59 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // Properties
+    
+    var count = 0
+    var label: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // MARK: Coordinates
+        
+        let width: CGFloat = 60
+        let height: CGFloat = 60
+        let xCenter: CGFloat = (self.view.frame.size.width - width)/2; // align horizontally |
+        let yCenter: CGFloat = (self.view.frame.size.height - height)/2 // align vertically  -
+        
+        
+        // MARK: Label
+        
+        let label = UILabel()
+        label.frame = CGRectMake(xCenter, 150, width, height)
+        label.text = "0"
+        
+        // text alignment
+        
+        // http://stackoverflow.com/questions/5686631/how-do-you-programatically-center-the-alignment-of-a-text-label-for-ios
+        
+        // https://developer.apple.com/library/ios/documentation/Cocoa/Reference/Foundation/Classes/NSString_Class/index.html#//apple_ref/swift/enum/c:@E@NSTextAlignment
+        
+        label.textAlignment = .Center
+        
+        self.view.addSubview(label)
+        self.label = label
+        
+        
+        // MARK: Button
+        
+        let button = UIButton()
+        
+        // http://stackoverflow.com/questions/12387988/how-to-programmatically-allign-a-button-in-ios-applications
+        
+        button.frame = CGRectMake(xCenter, yCenter, width, height)
+        button.setTitle("Click", forState: .Normal)
+        button.setTitleColor(UIColor.blueColor(), forState: .Normal)
+        self.view.addSubview(button)
+        
+        button.addTarget(self, action: #selector(ViewController.incrementCount), forControlEvents: UIControlEvents.TouchUpInside)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    
+    func incrementCount() {
+        self.count += 1
+        self.label.text = "\(self.count)"
     }
 
 
